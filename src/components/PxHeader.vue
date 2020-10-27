15781d51f5528133b904e11b6f70934e24c3fd28
juanpabloceliz <juanpablo.celiz@gmail.com>
2020-10-26 23:26:44 -0300

    almost ready in total

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -3,11 +3,11 @@
         <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
             <div class="flex items-center flex-shrink-0 text-white mr-6">
                 <PxIcon class="mr-4" />
-                <span class="font-semibold text-xl tracking-tight">Exchange</span>
+                <router-link class="font-semibold text-xl tracking-tight" to="/">Exchange</router-link>
             </div>
             <div class="hidden sm:block w-full md:block flex-grow-0 lg:flex lg:items-center lg:w-auto">
                 <div class="text-sm lg: flex-grow">
-                    <router-link to="/about" class="mt-5 text-xl text-green-600 hover:underline">About</router-link>
+                    <router-link to="/about" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">About</router-link>
                 </div>
             </div>
         </nav>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import '@/assets/css/tailwind.css'
 import router from '@/router'
 import { dollarFilter , percentFilter } from '@/filters'
+import Chartick from 'vue-chartkick'
+import Chart from 'chart.js'
+import { VueSpinners } from '@saeris/vue-spinners'
 
 Vue.filter('dollar', dollarFilter)
 Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
 
 new Vue({
   router: router,
